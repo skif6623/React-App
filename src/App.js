367e4e02fed221fb605bf31c831cc5f003be8f0c
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-10 22:56:10 +0200

    change path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import paintings from './paintings.json';
-import PaintingList from './components/PaintingList';
-import Section from './components/Section';
+import paintings from 'paintings';
+import PaintingList from 'components/PaintingList';
+import Section from 'components/Section';
 export default function App() {
   // const isOnline = true;
 
